15d4e2a410d77dcc124662984ae38e8f941858ef
potopus <paveldelo@yandex.ru>
2023-04-14 11:26:27 +0300

    feat: done 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActive) {
+  console.log(sampleActive);
+  if (typeof sampleActive !== 'string') return false;
+  if(sampleActive.indexOf(' ') >= 0) return false;
+  if (isNaN(sampleActive)) return false;
+  if (!isFinite(sampleActive))return false;
+  let numSampleActive = parseFloat(sampleActive)
+  if(numSampleActive<=0||numSampleActive>=15)return false;
+
+  let t = Math.log(MODERN_ACTIVITY/sampleActive)/(0.693/HALF_LIFE_PERIOD);
+console.log(t);
+console.log(typeof t);
+if (!isFinite(t))return false;
+console.log("done");
+  return Math.ceil(t)
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(mass) {
+  
+ 
+  return  mass.reduce((counterCats,closet) => {
+    return counterCats+closet.reduce((cats, someItem) => someItem==="^^"?cats+1:cats,0);
+      },0)
 }
 
 module.exports = {

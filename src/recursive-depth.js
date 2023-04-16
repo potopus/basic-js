6af960afc6cbe5979b68989ad2074971c3fd056a
potopus <paveldelo@yandex.ru>
2023-04-17 01:40:39 +0300

    feat: recursion done

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  calculateDepth(recurArr) {
+    console.log("BEGIN  :::")
+    // console.log(`arr: [ ${[...arr]}  ]`);
+    // let maxCount = 0;
+
+    // function recursion
+
+
+    let count = 0;
+    if (Array.isArray(recurArr) && recurArr.length === 1) {
+      count++;
+      recurArr.forEach((elem, ind, arr) => {//цикл для ОДНОГО элемента в массиве
+        // console.log(`elem   ${elem}   :  `);
+        if (Array.isArray(elem)) {//этот  ОДИН элемент массив?
+          count = count + this.calculateDepth(elem);
+        };
+      })
+    } else if (Array.isArray(recurArr) && recurArr.length === 0) {
+      count++
+    } else if (Array.isArray(recurArr) && recurArr.length > 1) {// БОЛШОЙ массив
+      count++
+      let maxDepth = 0;
+      recurArr.forEach((elem) => {
+        if (Array.isArray(elem)) {//этот  элемент БОЛЬШОГО МАССИВА сам тоже массив?
+          let elemDepth = this.calculateDepth(elem); //глубина i-го элемента
+          if (elemDepth > maxDepth) { maxDepth = elemDepth }
+        }
+      })
+      count = count + maxDepth;
+    }
+    // if (count > maxCount) { maxCount=count}
+    // else{maxCount};
+    // console.log("count:  " + count);
+    // console.log(maxCount);
+    // !end recursion
+    return count;
   }
 }
 
--- test/recursive-depth.test.js
+++ test/recursive-depth.test.js
@@ -14,12 +14,12 @@ const calculateDepth = instance.calculateDepth.bind(instance);
 const createFlatArr = (length) => Array.from({ length }, () => Math.floor(Math.random() * length));
 
 describe('Recursive depth', () => {
-    //Presence requirement
-    describe('variable presence', () => {
-        it.optional('function calculateDepth exists', () => {
-            expect(instance.calculateDepth).to.exist;
-        });
-    });
+    // //Presence requirement
+    // describe('variable presence', () => {
+    //     it.optional('function calculateDepth exists', () => {
+    //         expect(instance.calculateDepth).to.exist;
+    //     });
+    // });
 
     //Functional requirements
     describe('functional requirements', () => {
@@ -30,10 +30,13 @@ describe('Recursive depth', () => {
             }
         });
         it.optional('returns correct depth of nested arrays', () => {
-            assert.equal(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 31);
-            assert.equal(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 5);
+
+            // assert.equal(calculateDepth([[[]]]), 3);
+            assert.equal(calculateDepth([1, [8, [2]], 5, [[[]]]]), 4);
             assert.equal(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]), 3);
+            assert.equal(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
+            assert.equal(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 31);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 5);
             assert.equal(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
             assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 25);
             assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);

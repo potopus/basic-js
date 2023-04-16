feabf95083765428b44e90eaab47ca1990179745
potopus <paveldelo@yandex.ru>
2023-04-16 04:05:15 +0300

    feat: simple-chain done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  mass: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.mass.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.mass.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    // console.log("removeLink: " + position);
+    position= position -1;
+    // console.log("position--: " + position);
+    // console.log(parseInt(position) === position );
+    // console.log(position > 0 );
+    // console.log( position < this.mass.length);
+    // console.log("this.mass:  "+ this.mass);
+    // console.log("this.mass.length :"+ this.mass.length);
+    if (parseInt(position) === position && position >= 0 && position < this.mass.length) {
+      (this.mass).splice(position, 1);
+      return this;
+    } else { 
+      // console.log("eRROR");
+      this.mass=[];
+      throw new Error("You can't remove incorrect link!") }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // console.log("reverseChain()");
+    this.mass.reverse();
+    // console.log("reverse: " + [...this.mass]);
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // console.log("start finishChain()");
+    let result = this.mass.reduce((acc, elem, ind, arr) => {
+      if (ind !== arr.length - 1) {
+        return acc + `${elem} )~~( `
+      } else {
+        return acc + `${elem} )`
+      }
+    }, "( ");
+    this.mass = [];
+    // console.log("result: " + result);
+    return result
   }
+
 };
 
 module.exports = {

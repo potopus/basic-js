ab09d4992525f0860012d88fd0b662ec601632b5
potopus <paveldelo@yandex.ru>
2023-04-17 10:31:54 +0300

    feat:compleate extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  options.additionRepeatTimes = options.additionRepeatTimes >= 0 ? options.additionRepeatTimes : 1;
+  options.repeatTimes = options.repeatTimes >= 0 ? options.repeatTimes : 1;
+
+  options.separator = "" + (options.separator || '+');
+  options.additionSeparator = '' + (options.additionSeparator || '|');
+
+  str = "" + str;
+  options.addition =  typeof options.addition === "undefined"? "" : ""+options.addition ;
+
+
+  let fullString = '';
+
+  let fullAddition = '';
+  fullAddition = Array.from({ length: options.additionRepeatTimes }, (el) => { return options.addition }).join(options.additionSeparator);
+  console.log("FULLSTRING:    ", fullAddition);
+  fullString = Array.from({ length: options.repeatTimes }, (el,ind) => { return str + fullAddition });
+  fullString = fullString.join(options.separator);
+  console.log("FULLSTRING:    ", fullString)
+  return fullString
 }
 
 module.exports = {
   repeater
 };
+// let b = {
+//   repeatTimes: 4,
+//   separator: '1L467Kdqx2',
+//   addition: 'IMqCarClDg',
+//   additionRepeatTimes: 8,
+//   additionSeparator: 'U7L9D0f8pb'
+// }
--- test/extended-repeater.test.js
+++ test/extended-repeater.test.js
@@ -8,13 +8,13 @@ Object.freeze(expect);
 Object.freeze(assert);
 
 describe('Extended repeater', () => {
-  // Presence requirement
+  // Presence  requirement
 
-  describe('variable presence', () => {
-    it.optional('function repeater exists', () => {
-      expect(repeater).to.exist;
-    });
-  });
+  // describe('variable presence', () => {
+  //   it.optional('function repeater exists', () => {
+  //     expect(repeater).to.exist;
+  //   });
+  // });
 
   //Specific requirements
 
@@ -153,10 +153,10 @@ describe('Extended repeater', () => {
     });
 
     it.optional('supports different str & addition', () => {
+      assert.equal(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }), 'truefalse!!!false??? truefalse!!!false??? truefalse!!!false');
       assert.equal(repeater(9.234, { repeatTimes: 4, separator: '||', addition: { a: 5 }, additionRepeatTimes: 3, additionSeparator: '&&' }), '9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]');
       assert.equal(repeater(-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' }), '-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]');
       assert.equal(repeater(new Set(), { repeatTimes: 3, separator: '??? ', addition: [1, 2, 3, '4'], additionRepeatTimes: 2, additionSeparator: '!!!' }), '[object Set]1,2,3,4!!!1,2,3,4??? [object Set]1,2,3,4!!!1,2,3,4??? [object Set]1,2,3,4!!!1,2,3,4');
-      assert.equal(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }), 'truefalse!!!false??? truefalse!!!false??? truefalse!!!false');
       assert.equal(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }), 'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null');
     });
 
